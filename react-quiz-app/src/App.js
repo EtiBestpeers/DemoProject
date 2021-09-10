dd1aca39ad15986d40d5d7416e0f93990b9e5814
EtiBestpeers <etikhandelwal@bestpeers.com>
2021-09-10 22:28:13 +0530

    updates

--- react-quiz-app/src/App.js
+++ react-quiz-app/src/App.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer/Footer';
@@ -7,23 +9,46 @@ import Quiz from './Pages/Quiz/Quiz';
 import Result from './Pages/Result/Result';
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [questions, setQuestions] = useState();
+  const [score, setScore] = useState(0);
+
+
+  const fetchQuestions = async (category = "", difficulty = "") => {
+    const { data } = await axios.get(
+      `https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple`
+    );
+
+    setQuestions(data.results);
+  };
+
   return (
     <BrowserRouter>
-    <div className="app" style={{backgroundImage:'url(./ques1.png)'}}>
-     <Header/>
-     <switch>
-       <Route path='/'exact>
-         <Home/>
-       </Route>
-       <Route path='/quiz'exact>
-         <Quiz/>
-       </Route>
-       <Route path='/result'exact>
-         <Result/>
-       </Route>
-     </switch>
-    </div>
-    <Footer/>
+      <div className="app" style={{ backgroundImage: 'url(./ques1.png)' }}>
+        <Header />
+        <switch>
+          <Route path='/' exact>
+            <Home
+              name={name}
+              setName={setName}
+              fetchQuestions={fetchQuestions}
+            />
+          </Route>
+          <Route path='/quiz' exact>
+            <Quiz
+            name={name}
+            questions={questions}
+            score={score}
+            setscore={setScore}
+            setQuestions={setQuestions} />
+          </Route>
+          <Route path='/result' exact>
+            <Result />
+          </Route>
+        </switch>
+      </div>
+      <Footer />
     </BrowserRouter>
   );
 }
--- react-quiz-app/src/Pages/Home/Home.js
+++ react-quiz-app/src/Pages/Home/Home.js
@@ -1,18 +1,48 @@
 import {Button, MenuItem, TextField } from '@material-ui/core';
 import React from 'react';
 import './Home.css';
-import Categories, { } from "../../Data/Categories"
-function Home() {
+import Categories, { } from "../../Data/Categories";
+import {useState } from "react";
+import {useHistory} from "react-router"
+import ErrorMessage, {} from "../../components/ErrorMessage/ErrorMessage";
+const Home =({name ,setName, fetchQuestions}) => {
+
+    const [category, setCategory] = useState("");
+    const [difficulty, setDifficulty] = useState("");
+    const [error, seterror] = useState(false);
+    
+    const history = useHistory();
+
+    const handleSubmit = () => {
+ if(!category || !difficulty || !name){
+     seterror(true);
+     return ;
+ }
+ else{
+    seterror(false);
+    fetchQuestions(category, difficulty); 
+    history.push('/quiz')
+ }
+
+    };
+
     return (
         <div className="content">
             <div className="settings">
                 <span style={{ fontSize: 30 }}>Quiz Details</span>
+
                 <div className='settings_select'>
-                    <TextField style={{ marginBottom: 25 }} label="Enter Your Name" variant="outlined" />
+                    {error && <ErrorMessage> PLease fill all the feilds</ErrorMessage>}
+                    <TextField style={{ marginBottom: 25 }} label="Enter Your Name" variant="outlined" 
+                    onChange={(e) => setName(e.target.value)} 
+                    
+                    />
                     <TextField
                         select label="Select Category"
                         variant="outlined"
                         style={{ marginBottom: 30 }}
+                        onChange={(e) => setCategory(e.target.value)}
+                        value={category}
                     >
                         {
                             Categories.map((cat) => (
@@ -26,15 +56,19 @@ function Home() {
                         select label="Select Difficulty"
                         variant="outlined"
                         style={{ marginBottom: 30 }}
+                        onChange={(e) => setDifficulty(e.target.value)}
+                        value={difficulty}
                     >
                         <MenuItem key="Easy" value="easy">Easy</MenuItem>
                         <MenuItem key="Medium" value="medium">Medium</MenuItem>
                         <MenuItem key="Hard" value="hard">Hard</MenuItem>
                     </TextField>
-                    <Button variant="contained" color="primary">Start Quiz</Button>
+                    <Button variant="contained" color="primary" size="large"
+                    onClick={handleSubmit}
+                    >Start Quiz</Button>
                 </div>
             </div>
-            <img src="/quiz.jpeg" className='banner' alt="quiz img" />
+            <img src="/banner.jpg" className='banner' alt="quiz img" />
         </div>
     );
 };
